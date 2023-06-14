68b582888551e3adc7459729f2c0ce7fc5fe688e
John Anderson <djanyreason@gmail.com>
2023-06-14 12:50:57 -0400

    Completes exercise 1.8 by adding Statistics comp.
    This commit adds a separate Statistics component which takes good,
    neutral, and bad as props and returns HTML for the entire statistics
    section.

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,5 +1,22 @@
 import { useState } from 'react'
 
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad;
+  const score = good - bad;
+
+  return (
+    <div>
+      <h3>statistics</h3>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {score / total} </p>
+      <p>positive {100 * (good / total)} % </p>
+    </div>
+  );
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -11,17 +28,13 @@ const App = () => {
 
   return (
     <div>
-      <h3>give feedback</h3>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h3>statistics</h3>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {score / total} </p>
-      <p>positive {100 * (good / total)} % </p>
+      <div>
+        <h3>give feedback</h3>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
