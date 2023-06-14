b27e1afe654c9d068afb82be0b2b63d0ce39216d
John Anderson <djanyreason@gmail.com>
2023-06-14 12:37:05 -0400

    Completes exercise 1.6
    This commit adds HTML/react to the return portion of App() so that it
    contains a section consisting of 3 buttons, and a section showing how
    many times each button has been pressed.

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -8,7 +8,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h3>give feedback</h3>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h3>statistics</h3>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
